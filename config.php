80e39e4ec0fcff58261339759b99b099f0292c92
Nathan Phung <nphung@usc.edu>
2015-10-31 10:52:25 -0700

    Added masking for input and library files in resources

--- config.php
+++ config.php
@@ -5,8 +5,9 @@
 	define( "DB_DSN", "mysql:host=uscitp.com;dbname=tbian_LACounty" ); // Constant that tells PHP where to find MySQL database
 	define( "DB_USERNAME", "tbian_test" );
 	define( "DB_PASSWORD", "btbtbt@9" );
-	define( "CLASS_PATH", "classes" ); // Path in relation to top-level folder, cms
-	define( "TEMPLATE_PATH", "templates" ); // Path in relation to top-level folder, cms
+	define( "CLASS_PATH", "classes/" ); // Path in relation to top-level folder, cms
+	define( "TEMPLATE_PATH", "templates/" ); // Path in relation to top-level folder, cms
+	define( "RESOURCE_PATH", "resources/")
 	require( CLASS_PATH . "/PropertyInfo.php" );
 
 	function handleException( $exception ) {
