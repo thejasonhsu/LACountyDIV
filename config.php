fb89e7f5b34aa2f8f036b39c933303116109ccf0
Jenny Yoshihara <jyoshiha@usc.edu>
2015-10-31 11:13:01 -0700

    Added initial backend logic, added form tags to landing

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 	ini_set( "display_errors", true ); // Causes error messages to be displayed in the browser; turn off on live site
 	date_default_timezone_set( "America/Los_Angeles" );  // http://www.php.net/manual/en/timezones.php
-	// TODO: Replace name with actual database name
+	
 	define( "DB_DSN", "mysql:host=uscitp.com;dbname=tbian_LACounty" ); // Constant that tells PHP where to find MySQL database
 	define( "DB_USERNAME", "tbian_test" );
 	define( "DB_PASSWORD", "btbtbt@9" );
 	define( "CLASS_PATH", "classes/" ); // Path in relation to top-level folder, cms
-	define( "TEMPLATE_PATH", "templates/" ); // Path in relation to top-level folder, cms
+	define( "TEMPLATE_PATH", "pages/" ); // Path in relation to top-level folder, cms
 	define( "RESOURCE_PATH", "resources/")
 	require( CLASS_PATH . "/PropertyInfo.php" );
 
--- index.php
+++ index.php
@@ -1,7 +1,64 @@
 <?php
 	require( "config.php" );
-	$array = PropertyInfo::getInfoUsingId(2023007025);
+
+	session_start();
+	$action = isset( $_GET['action'] ) ? $_GET['action'] : "";
+	$username = isset( $_SESSION['username'] ) ? $_SESSION['username'] : "";
+
+	echo $action;
+
+	switch ( $action ) {
+		case 'login':
+			login();
+			break;
+		case 'form':
+			form();
+		case 'confirm':
+			confirm();
+		case 'submit':
+			submit();
+		default:
+			landingpage();
+	}
+
+	function login() {
+		// Check for valid AIN and PIN match
+		$validMatch = true;
+		$captchaResponse = true;
+
+		if ($validMatch && $captchaResponse) {
+			// Get info from database
+
+			form();
+		}
+		else {
+			// Show that it was an invalid combination
+		}
+	}
+
+	function logout() {
+		unset( $_SESSION['username'] );
+		// Is there a formal logout? Where do we redirect them?
+	}
+
+	function form() {
+		require( TEMPLATE_PATH . "form.html" );
+	}
+
+	function confirm() {
+
+	}
+
+	function submit() {
+		
+	}
+
+	function landingpage() {
+		require( TEMPLATE_PATH . "landing.html" );
+	}
+
+	/*$array = PropertyInfo::getInfoUsingId(2023007025);
 	print_r($array);
 	echo "The AIN for the record requested is: ";
-	echo $array['AIN'];
+	echo $array['AIN'];*/
 ?>
